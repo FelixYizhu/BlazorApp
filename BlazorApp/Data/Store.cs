7de1fcd9631b383fb2b0f4e351f01c110808b4aa
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-08-24 02:31:31 +1000

    Add files via upload

--- BlazorApp/Data/Store.cs
+++ BlazorApp/Data/Store.cs
@@ -1,1 +1,28 @@
+﻿using BlazorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace BlazorApp.Data
+{
+    public class Store
+    {
+        private List<Student> _students;
+
+        public void SetStudents(List<Student> list)
+        {
+            _students = list;
+        }
+        public List<Student> GetStudents()
+        {
+            return _students;
+        }
+
+        public Student GetStudentById(int id)
+        {
+            var stu = _students.SingleOrDefault(x => x.Id == id);
+            return stu;
+        }
+    }
+}
