56c6f90508fd7f64745c11e2f19e5a95b5f2e781
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-08-25 00:22:49 +1000

    Add files via upload

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BlazorApp.Data;
 
 namespace BlazorApp
 {
@@ -17,7 +18,9 @@ namespace BlazorApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44342") });
+
+            builder.Services.AddSingleton<Store>();
 
             await builder.Build().RunAsync();
         }
